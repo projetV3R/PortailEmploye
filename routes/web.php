be73ee459dbf526c6ffe7489b24d0f354a0f5a6a
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-09 10:27:59 -0400

    V3R-E Modification nom controller et route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\UsagerController;
 
 Route::get('/', function () {
     return view('welcome');
