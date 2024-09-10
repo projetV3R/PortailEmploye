189f9fa782b3ada0d7a1d29f35fb85c57f0f7f8b
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-10 09:30:24 -0400

    V3R-E-Ajout menu mobile

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -4,46 +4,77 @@
 
 @section('contenu')
 
-<div class="flex flex-col h-screen">
+<div class="flex flex-col md:flex-row h-screen">
 
-    <div class="flex flex-col justify-center h-full w-72 ">
-    <div id="arrow-icon" class="absolute  left-72 transition-all duration-300 hover:animate-bounce-horizontal cursor-pointer">
-        <span class="iconify text-3xl" data-icon="material-symbols:arrow-forward-ios-rounded" data-inline="false"></span>
+
+    <div class="md:hidden flex justify-center bg-gray-300 w-full">
+        <button class="bg-gray-300 p-4" id="dropdownToggle">
+            <span class="iconify text-xl w-8" data-icon="material-symbols:menu" data-inline="false"></span>
+        </button>
     </div>
-        <div class="flex flex-col items-center bg-gray-300 rounded-md shadow-md w-full ">
 
-            <div class="flex flex-row cursor-pointer hover:bg-gray-200  p-4 px-2 w-full justify-start items-center gap-3 menu" onclick="SelectedMenu(this)">
-                <span class="iconify text-3xl w-8" data-icon="material-symbols:format-list-bulleted" data-inline="false"></span>
+  
+    <div class="hidden md:flex flex-col justify-center w-full md:w-64 h-1/2 md:h-full" id="menuDropdown">
+        <div class="flex flex-col items-center bg-gray-300 w-full justify-center h-full">
+
+            <div class="flex flex-row cursor-pointer hover:bg-gray-200 p-4 px-2 w-full justify-center md:justify-start items-center gap-3 menu" data-target="usersDiv" onclick="SelectedMenu(this)">
+                <span class="iconify text-xl w-8" data-icon="material-symbols:format-list-bulleted" data-inline="false"></span>
                 <span class="text-xl">Utilisateurs</span>
-                <span class="iconify text-3xl w-6 hidden arrow" data-icon="material-symbols:arrow-forward-ios-rounded"></span>
+                <span class="iconify text-xl w-6 hidden arrow" data-icon="material-symbols:arrow-forward-ios-rounded"></span>
             </div>
 
-            <div class="flex flex-row cursor-pointer hover:bg-gray-200  p-4 px-2 w-full justify-start items-center gap-3 menu" onclick="SelectedMenu(this)">  
-                <span class="iconify text-3xl w-8" data-icon="material-symbols:box-outline-rounded" data-inline="false"></span>
+            <div class="flex flex-row cursor-pointer hover:bg-gray-200 p-4 px-2 w-full justify-center md:justify-start items-center gap-3 menu" data-target="suppliersDiv" onclick="SelectedMenu(this)">
+                <span class="iconify text-xl w-8" data-icon="material-symbols:box-outline-rounded" data-inline="false"></span>
                 <span class="text-xl">Fournisseurs</span>
-                <span class="iconify text-3xl w-6 hidden arrow" data-icon="material-symbols:arrow-forward-ios-rounded"></span>
+                <span class="iconify text-xl w-6 hidden arrow" data-icon="material-symbols:arrow-forward-ios-rounded"></span>
             </div>
 
-            <div class="flex flex-row cursor-pointer hover:bg-gray-200 p-4 px-2 w-full justify-start items-center gap-3 menu" onclick="SelectedMenu(this)">
-                <span class="iconify text-3xl w-8" data-icon="material-symbols:display-settings-outline-rounded" data-inline="false"></span>
+            <div class="flex flex-row cursor-pointer hover:bg-gray-200 p-4 px-2 w-full justify-center md:justify-start items-center gap-3 menu" data-target="settingsDiv" onclick="SelectedMenu(this)">
+                <span class="iconify text-xl w-8" data-icon="material-symbols:display-settings-outline-rounded" data-inline="false"></span>
                 <span class="text-xl">Paramètres</span>
-                <span class="iconify text-3xl w-6 hidden arrow " data-icon="material-symbols:arrow-forward-ios-rounded"></span>
+                <span class="iconify text-xl w-6 hidden arrow" data-icon="material-symbols:arrow-forward-ios-rounded"></span>
             </div>
         </div>
-
     </div>
+
+
+    <div class="flex flex-col justify-center w-full h-full bg-red-500" id="usersDiv"></div>
+    <div class="flex flex-col justify-center w-full h-full bg-yellow-500 hidden" id="suppliersDiv"></div>
+    <div class="flex flex-col justify-center w-full h-full bg-blue-500 hidden" id="settingsDiv"></div>
 
 </div>
 
 <script>
+
+    document.getElementById('dropdownToggle').addEventListener('click', function() {
+        var menu = document.getElementById('menuDropdown');
+        if (menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+        } else {
+            menu.classList.add('hidden');
+        }
+    });
+
+
     function SelectedMenu(element) {
         let items = document.querySelectorAll('.menu');
+        let targetId = element.getAttribute('data-target');
+
+   
         items.forEach(item => {
             item.classList.remove('bg-gray-200');
             item.querySelector('.arrow').classList.add('hidden');
         });
+
+    
+        let contentDivs = document.querySelectorAll('[id$="Div"]');
+        contentDivs.forEach(div => {
+            div.classList.add('hidden');
+        });
+
         element.classList.add('bg-gray-200');
         element.querySelector('.arrow').classList.remove('hidden');
+        document.getElementById(targetId).classList.remove('hidden');
     }
 </script>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,7 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        'bounce-horizontal': {
-          '0%, 100%': { transform: 'translateX(0)' },
-          '50%': { transform: 'translateX(25%)' },
-        },
-      },
-      animation: {
-        'bounce-horizontal': 'bounce-horizontal 1s infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
