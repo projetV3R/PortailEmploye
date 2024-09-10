ab27b85335a295fe7610acae13f24f4612abe717
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-10 09:31:20 -0400

    V3R-E Ajout navIcon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
   <title>@yield('title')</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://code.iconify.design/3/3.0.0/iconify.min.js"></script>
-
+  <link rel="icon" type="image/png" href="https://www.v3r.net/wp-content/uploads/2023/06/favicon.png" />
   @vite('resources/css/app.css') 
   <link rel="stylesheet" href="{{ asset('style.css') }}">
   
