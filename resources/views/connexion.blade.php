60d6e0bbde6439aaf932303e48aa85374a75ec4c
Noua Sedira <nsedira7@gmail.com>
2024-09-10 10:08:00 -0400

    MAJ

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -7,7 +7,7 @@
 
 <div class="flex flex-col md:flex-row gap-10 p-4 md:p-10">
     <div class="flex-1 max-w-full md:max-w-[50%]">
-        <div class="pt-10 pl-4 md:pl-10">
+        <div class="flex flex-col items-center justify-center p-4">
             <strong>
                 <h1 class="text-3xl md:text-4xl">Bon Retour!</h1>
             </strong>
@@ -16,18 +16,26 @@
             </p>
         </div>
 
-        <div class="bg-cyan-50">
+        <div class="flex flex-col items-center justify-center p-4 md:p-10">
             <form action="">
-                <div class="p-4 md:p-10">
+                <div class="p-4">
                     <label for="neq" class="form-label block mb-2"><strong>Numéro d'entreprise du Québec (NEQ)</strong></label>
-                    <input type="text" name="neq" id="neq" autocomplete="neq" class="block w-full md:w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-blue-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Votre NEQ">
+                    <input type="text" name="neq" id="neq" autocomplete="neq" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-blue-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Votre NEQ">
                     <a class="text-blue-500 hover:underline text-xs" href="#">Pas de NEQ?</a>
                 </div>   
 
-                <div class="p-4 md:p-10">
+                <div class="p-4">
                     <label for="password" class="form-label block mb-2"><strong>Votre mot de passe</strong></label>
-                    <input type="password" name="password" id="password" autocomplete="password" class="block w-full md:w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-blue-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Votre mot de passe">
+                    <input type="password" name="password" id="password" autocomplete="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-blue-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Votre mot de passe">
                         <a class="text-blue-500 hover:underline text-xs text-right" href="#">Mot de passe oublié?</a>
+                </div>
+                <div class="p-4">
+                    <button type="submit" class="block w-full py-2 px-4 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm">
+                       <strong>Suivant</strong>
+                    </button>
+                    <a class="hover:underline text-xs text-right" href="#">Première connexion ou NEQ non trouvée? <br> 
+                        <strong class="text-blue-500">Soumettre une demande !</strong> 
+                    </a>
                 </div>
             </form> 
         </div>
