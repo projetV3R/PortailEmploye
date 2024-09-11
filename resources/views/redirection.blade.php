5df7ceaeadf11c46a23e276215acfa9dd0dd10de
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-10 20:03:37 -0400

    wip redirection
    -responsive laisse à désirer

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -5,12 +5,12 @@
 @section('contenu')
 
 <div class="w-full p-6">
-    <a href="/" class="relative z-10 basis-1/6">
+    <a href="/" class="md:1/2 sm:1/3 relative z-10 basis-1/6">
         <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
     </a>
     <div class="flex flex-row">
         <b>
-            <h1 class="relative z-10 w-1/2 m-40 text-center text-2xl">Vous n'avez pas accès à cette page veuillez retourner au menu principal.</h1>
+            <h1 class="sm:w-96 md:w-120 md:m-20 lg:w-1/2 relative z-10 m-40 text-center text-2xl">Vous n'avez pas accès à cette page veuillez retourner au menu principal.</h1>
         </b>
         <div class="absolute inset-0">
             <img src="{{asset('images/Vector.png')}}" alt="" class="absolute w-3/4 h-auto right-0 z-0">
