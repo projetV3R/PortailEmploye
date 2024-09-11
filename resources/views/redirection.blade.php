31da5dc20cf3636929776e6a9f4df462209938ab
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-11 11:46:13 -0400

    wip redirection
    -responsive presque fini
    -manque site yousouf pour nouv image

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -9,15 +9,22 @@
         <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
     </a>
     <div class="flex flex-row">
-        <b>
-            <h1 class="w-1/2 relative z-10 m-40 text-center text-2xl">Vous n'avez pas accès à cette page veuillez retourner au menu principal.</h1>
-        </b>
+        <div class="relative w-3/4 lg:ml-40 md:ml-20 sm:ml-10">
+            <div class="w-full lg:w-3/5 md:w-3/4 sm:w-2/3 lg:text-2xl md:text-lg sm:text-sm ">
+                <h1 class="relative z-10 text-center p-10 mt-6">
+                    <b>Vous n'avez pas accès à cette page veuillez retourner au menu principal.</b>
+                </h1>
+            </div>
+        </div>
         <div class="absolute inset-0">
-            <img src="{{asset('images/Vector.png')}}" alt="" class="absolute sm:w-full md:w-3/4 lg:3/4 h-auto right-0 z-0">
-            <img src="{{asset('images/gifCollab.gif')}}" alt="" class="absolute top-0 right-0 h-auto z-0">
+            <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
+                <img src="{{asset('images/Vector.png')}}" alt="" class="absolute h-auto right-0 z-0">
+            </div>
+            <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
+                <img src="{{asset('images/gifCollab.gif')}}" alt="" class="absolute top-0 right-0 h-auto z-0">
+            </div>
         </div>
     </div>
-
 </div>
 
 
