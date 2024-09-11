4365672ac5f91ed3e48db150b2d068410c60e30d
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-11 14:40:59 -0400

    Update redirection.blade.php
    -responsive ok
    -manque nouv image.

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -10,7 +10,7 @@
     </a>
     <div class="flex flex-row">
         <div class="relative">
-            <div class="w-full lg:w-3/5 md:w-3/4 sm:w-2/3 lg:text-2xl md:text-lg sm:text-sm lg:ml-40 md:ml-20 sm:ml-10 sm:mt-10">
+            <div class="w-full lg:w-3/5 md:w-3/4 sm:w-2/3 lg:text-2xl md:text-xl sm:text-md lg:ml-40 md:ml-20 sm:ml-10 sm:mt-10">
                 <h1 class="relative z-10 text-center p-10 mt-6">
                     <b>Vous n'avez pas accès à cette page veuillez retourner au menu principal.</b>
                 </h1>
@@ -20,8 +20,8 @@
             <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
                 <img src="{{asset('images/Vector.png')}}" alt="" class="absolute h-auto right-0 z-0">
             </div>
-            <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
-                <img src="{{asset('images/gifCollab.gif')}}" alt="" class="absolute top-0 right-0 h-auto z-0">
+            <div class="w-auto">
+                <img src="{{asset('images/gifCollab.gif')}}" alt="" class="absolute top-0 right-0 z-0 h-lvh">
             </div>
         </div>
     </div>
