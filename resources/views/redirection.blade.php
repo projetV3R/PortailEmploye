3c7e63c20c84b58af30adebc672c4f3cf5c65522
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-15 17:37:00 -0400

    update redirection
    tjrs bug avec espace blanc lorsque sm...

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -26,11 +26,11 @@
         </div>
     </div>
     <div class="absolute inset-0">
-        <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
-            <img src="{{ asset('images/Vector.png') }}" alt="" class="absolute h-screen right-0 z-0 lg:hidden">
+        <div class="w-full">
+            <img src="{{ asset('images/Vector.png') }}" alt="" class="absolute h-screen lg:hidden">
         </div>
-        <div class="w-full lg:w-3/4 md:w-2/3 sm:w-2/3 flex items-center justify-center h-screen">
-            <img src="{{ asset('images/403_Error_Forbidden.gif') }}" alt="imageErreur403" class="absolute right-0 z-0 lg:hidden">
+        <div class="w-full flex items-center h-screen">
+            <img src="{{ asset('images/403_Error_Forbidden.gif') }}" alt="imageErreur403" class="absolute right-0  lg:hidden">
         </div>
     </div>
 
