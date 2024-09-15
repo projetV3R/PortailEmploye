b587ac0533d379ca60bfb360792911b3e9e57206
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-15 17:02:50 -0400

    wip

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -4,25 +4,25 @@
 
 @section('contenu')
 
-<div class="w-full p-6">
-    <a href="/" class="w-auto relative z-10">
-        <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
-    </a>
-    <div class="flex flex-row">
-        <div class="relative">
-            <div class="w-full lg:w-3/5 md:w-3/4 sm:w-2/3 lg:text-2xl md:text-xl sm:text-md lg:ml-40 md:ml-20 sm:ml-10 sm:mt-10">
-                <h1 class="relative z-10 text-center p-10 mt-6">
-                    <b>Vous n'avez pas accès à cette page veuillez retourner au menu principal.</b>
-                </h1>
-            </div>
+<div class="flex flex-row h-lvh">
+    <div class="absolute left-0 top-0">
+        <div class="relative inset-x-0 z-10">
+            <a href="/" class="">
+                <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
+            </a>
         </div>
-        <div class="absolute inset-0">
-            <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
-                <img src="{{asset('images/Vector.png')}}" alt="" class="absolute h-auto right-0 z-0">
-            </div>
-            <div class="w-auto">
-                <img src="{{asset('images/gifCollab.gif')}}" alt="" class="absolute top-0 right-0 z-0 h-lvh">
-            </div>
+        <div class="w-full relative inset-x-0 z-10 lg:text-2xl md:text-xl sm:text-md ml-20 mr-20">
+            <h1 class="text-center p-2">
+                <b>Vous n'avez pas accès à cette page.</b>
+            </h1>
+        </div>
+    </div>
+    <div class="absolute inset-0">
+        <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
+            <img src="{{asset('images/Vector.png')}}" alt="" class="absolute h-auto right-0 z-0">
+        </div>
+        <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
+            <img src="{{asset('images/403_Error_Forbidden.gif')}}" alt="imageErreur403" class="absolute top-0 right-0 z-0 h-vhl">
         </div>
     </div>
 </div>
