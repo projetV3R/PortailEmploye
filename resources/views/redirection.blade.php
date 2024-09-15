eba577665384bd9f4863a985efa27dee1e07e590
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-15 17:30:49 -0400

    update redirection
    - responsive presque parfait manque de retirer espace blanc qui gène lorsque sm

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -11,13 +11,13 @@
                 <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
             </a>
         </div>
-        <div class="w-full relative inset-x-0 z-10 lg:text-2xl md:text-xl sm:text-md ml-20 mr-20">
-            <h1 class="text-center p-2">
+        <div class="w-full relative inset-x-0 z-10 text-2xl m-20">
+            <h1 class="text-center p-6">
                 <b>Vous n'avez pas accès à cette page.</b>
             </h1>
         </div>
     </div>
-    <div class="absolute inset-0">
+    <div class="absolute inset-0 hidden md:block">
         <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
             <img src="{{asset('images/Vector.png')}}" alt="" class="absolute h-auto right-0 z-0">
         </div>
@@ -25,6 +25,15 @@
             <img src="{{asset('images/403_Error_Forbidden.gif')}}" alt="imageErreur403" class="absolute top-0 right-0 z-0 h-vhl">
         </div>
     </div>
+    <div class="absolute inset-0">
+        <div class="w-full lg:w-3/4 md:w-2/3 sm:w-1/2">
+            <img src="{{ asset('images/Vector.png') }}" alt="" class="absolute h-screen right-0 z-0 lg:hidden">
+        </div>
+        <div class="w-full lg:w-3/4 md:w-2/3 sm:w-2/3 flex items-center justify-center h-screen">
+            <img src="{{ asset('images/403_Error_Forbidden.gif') }}" alt="imageErreur403" class="absolute right-0 z-0 lg:hidden">
+        </div>
+    </div>
+
 </div>
 
 
