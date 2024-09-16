b4b576a53801c14693c00414978fe8be7edb27b7
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-09-16 09:01:07 -0400

    update redirection
    Ajout d'un fond à l'image.

--- resources/views/redirection.blade.php
+++ resources/views/redirection.blade.php
@@ -4,26 +4,23 @@
 
 @section('contenu')
 
-<div class="">
-    <div class="absolute left-0 top-0">
-        <div class="relative inset-x-0 z-10">
-            <a href="/" class="">
-                <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
-            </a>
-            <h1 class="m-20 text-2xl text-center">
-                <b>Vous n'avez pas accès à cette page.</b>
-            </h1>
-        </div>
+<div class="w-full">
+    <div class="absolute inset-x-0 z-10">
+        <a href="/" class="">
+            <span class="iconify size-10" data-icon="ion:arrow-undo" data-inline="false"></span>
+        </a>
+        <h1 class="m-10 text-2xl text-center">
+            <b>Vous n'avez pas accès à cette page.</b>
+        </h1>
     </div>
-    <div class="inset-0">
-        <div class="w-full">
-            <img src="{{ asset('images/Vector.png') }}" alt="" class="absolute h-screen right-0">
-            <div class="flex items-center h-screen">
-                <img src="{{ asset('images/403_Error_Forbidden.gif') }}" alt="imageErreur403" class="absolute h-auto right-0">
-            </div>
-        </div>
+    <div class="relative h-screen flex items-center justify-center overflow-hidden">
+        <img src="{{ asset('images/bg_redirection1.png') }}" alt="imagebackground" class="absolute inset-0 object-cover h-full w-full">
+        <img src="{{ asset('images/403_Error_Forbidden.gif') }}" alt="imageErreur403" class="relative z-10 h-auto max-w-full">
     </div>
+
 </div>
+
+
 
 
 @endsection
