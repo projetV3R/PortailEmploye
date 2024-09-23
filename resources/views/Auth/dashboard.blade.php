14aabaa64454561749fa04ea66d44064cf516a28
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-23 08:59:33 -0400

    Squashed commit of the following:
    commit ccb47cc604079b9efc5e5b7c115dab6516228d9d
    Merge: bf2105c c650708
    Author: Nathan Michel <143432891+MN97490@users.noreply.github.com>
    Date:   Mon Sep 23 08:58:55 2024 -0400
    
        Merge pull request #8 from projetV3R/V3R-VA-18/feature-
    
        V3 r va 18/feature
    
    commit c6507085d9e94d8f6bfca966eb7afe8daa2559cd
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 08:57:26 2024 -0400
    
        Responsive fait
    
    commit ace6b20e1d35c2ba95282b482cc1d40cfb0129ab
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 08:17:33 2024 -0400
    
        Ajout emplacement listes fournisseurs et emplacement insc/tp/filter
    
    commit f5e5448a388c0ae3b46aee84a2abbada3529997a
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Sun Sep 22 22:53:46 2024 -0400
    
        Ajout debut layout page d'accueil section charts debut manque responsive

--- resources/views/Auth/dashboard.blade.php
+++ resources/views/Auth/dashboard.blade.php
@@ -1,14 +1,34 @@
 @extends('layouts.app')
  
-@section('title', "Page d'acceuil employer")
+@section('title', "Accueil")
  
 @section('contenu')
 
 @auth
+<div class="flex flex-col w-full h-full p-4 lg:p-16 gap-y-4 ">
+<div class="flex w-full flex-col lg:flex-row lg:h-3/4 h-full gap-4">
+<div class="flex lg:w-1/2 w-full h-full">
+    <div class="flex w-full justify-center border-2 border-dashed ">HIGHCHARTS INSCRIPTION </div> 
+</div>
+<div class="flex lg:w-1/2 w-full h-full">
+     <div class="flex w-full border-2 border-dashed justify-center ">HIGHCHARTS CATEGORIE INSCRIPTION </div> </div>
+</div>
+
+<div class="flex w-full ">
+    <div class="flex w-full h-36 border-2 border-dashed justify-center">
+        DERNIERE INSCRIPTION ENREGISTRER OU TIMEPICKER POUR LES CHARTS OU FILTRE POUR LES LISTE ICI
+    </div>
+</div>
+
+<div class="flex w-full">
+    <div class="flex w-full justify-center border-2 border-dashed">LISTES FOURNISSEURS
+    </div>
+</div>
+
+</div>
+
     
-        <div>
-            <h1>Connecter</h1>
-        </div>
+
 @endauth
 
 @endsection
