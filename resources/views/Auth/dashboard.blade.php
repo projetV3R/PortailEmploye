ace6b20e1d35c2ba95282b482cc1d40cfb0129ab
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-23 08:17:33 -0400

    Ajout emplacement listes fournisseurs et emplacement insc/tp/filter

--- resources/views/Auth/dashboard.blade.php
+++ resources/views/Auth/dashboard.blade.php
@@ -5,12 +5,27 @@
 @section('contenu')
 
 @auth
-<div class="flex w-full h-full p-4 lg:p-16 ">
+<div class="flex flex-col w-full h-full p-4 lg:p-16 gap-y-4 ">
 <div class="flex w-full h-3/4 gap-4">
-<div class="flex w-1/2  "><div class="flex w-full justify-center border-2 border-dashed ">HIGHCHARTS INSCRIPTION </div> </div>
-<div class="flex w-1/2 "> <div class="flex w-full border-2 border-dashed justify-center   ">HIGHCHARTS CATEGORIE INSCRIPTION </div </div>
-
+<div class="flex w-1/2  ">
+    <div class="flex w-full justify-center border-2 border-dashed ">HIGHCHARTS INSCRIPTION </div> 
 </div>
+<div class="flex w-1/2 ">
+     <div class="flex w-full border-2 border-dashed justify-center ">HIGHCHARTS CATEGORIE INSCRIPTION </div> </div>
+</div>
+
+<div class="flex w-full ">
+    <div class="flex w-full h-40 border-2 border-dashed justify-center">
+        DERNIERE INSCRIPTION ENREGISTRER OU TIMEPICKER POUR LES CHARTS OU FILTRE POUR LES LISTE ICI
+    </div>
+</div>
+
+<div class="flex w-full">
+    <div class="flex w-full justify-center border-2 border-dashed">LISTES FOURNISSEURS
+    </div>
+</div>
+</div>
+
     
 
 @endauth
