c6507085d9e94d8f6bfca966eb7afe8daa2559cd
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-23 08:57:26 -0400

    Responsive fait

--- resources/views/Auth/dashboard.blade.php
+++ resources/views/Auth/dashboard.blade.php
@@ -6,16 +6,16 @@
 
 @auth
 <div class="flex flex-col w-full h-full p-4 lg:p-16 gap-y-4 ">
-<div class="flex w-full h-3/4 gap-4">
-<div class="flex w-1/2  ">
+<div class="flex w-full flex-col lg:flex-row lg:h-3/4 h-full gap-4">
+<div class="flex lg:w-1/2 w-full h-full">
     <div class="flex w-full justify-center border-2 border-dashed ">HIGHCHARTS INSCRIPTION </div> 
 </div>
-<div class="flex w-1/2 ">
+<div class="flex lg:w-1/2 w-full h-full">
      <div class="flex w-full border-2 border-dashed justify-center ">HIGHCHARTS CATEGORIE INSCRIPTION </div> </div>
 </div>
 
 <div class="flex w-full ">
-    <div class="flex w-full h-40 border-2 border-dashed justify-center">
+    <div class="flex w-full h-36 border-2 border-dashed justify-center">
         DERNIERE INSCRIPTION ENREGISTRER OU TIMEPICKER POUR LES CHARTS OU FILTRE POUR LES LISTE ICI
     </div>
 </div>
@@ -24,6 +24,7 @@
     <div class="flex w-full justify-center border-2 border-dashed">LISTES FOURNISSEURS
     </div>
 </div>
+
 </div>
 
     
