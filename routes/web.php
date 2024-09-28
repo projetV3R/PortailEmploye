0231ec315af0e74b68ef341fcecc0728b2b34f79
Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
2024-09-27 22:40:42 -0400

    Squashed commit of the following:
    commit faf3794bd1faec1c7bdb0a2aa1ae29be0d09160c
    Merge: 65320e7 dc06c5f
    Author: Nathan Michel <143432891+MN97490@users.noreply.github.com>
    Date:   Fri Sep 27 22:36:52 2024 -0400
    
        Merge pull request #10 from projetV3R/V3R-VA-76/feature-
    
        V3 r va 76/feature
    
    commit dc06c5f11128e5d1c27c44aa09a12db62d4eb380
    Merge: a84f3f8 65320e7
    Author: Nathan Michel <143432891+MN97490@users.noreply.github.com>
    Date:   Fri Sep 27 22:36:40 2024 -0400
    
        Merge branch 'Staging-Nat' into V3R-VA-76/feature-
    
    commit a84f3f8f78570672c5e91d32525b28a09ae8ba46
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Fri Sep 27 22:35:38 2024 -0400
    
        Mise a jour dossier redirection et ajout route de redirection error 404
    
    commit 65320e7fc6d61fc209b7f5f0492dd96bf2054d79
    Merge: ccb47cc 62678d4
    Author: Nathan Michel <143432891+MN97490@users.noreply.github.com>
    Date:   Mon Sep 23 20:41:01 2024 -0400
    
        Merge pull request #9 from projetV3R/V3R-VA-61/feature-
    
        V3 r va 61/feature
    
    commit 62678d4727509cf55c8359265c50598fc8a87703
    Merge: e1e8fc2 ccb47cc
    Author: Nathan Michel <143432891+MN97490@users.noreply.github.com>
    Date:   Mon Sep 23 20:40:46 2024 -0400
    
        Merge branch 'Staging-Nat' into V3R-VA-61/feature-
    
    commit e1e8fc20cd880aa731862e904fafdd6d3cdcd398
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 20:37:38 2024 -0400
    
        Finalisation menu responsive mobile et securisation auth et role
    
    commit 4b2c54a0fa0fa82f4906ffe6e2a0d1761528aa13
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 15:50:15 2024 -0400
    
        Ajout dark mode bouton de deconnexion et animation sur le hover bouton
    
    commit 384809e82944f40ad5afb105bcda43b5e69df4a2
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 11:15:47 2024 -0400
    
        update responsive menu manque ajout des liens
    
    commit 214e7bc81a896b23bd8efcb0655ba6633cd5594b
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 10:35:03 2024 -0400
    
        Update header
    
    commit ccb47cc604079b9efc5e5b7c115dab6516228d9d
    Merge: bf2105c c650708
    Author: Nathan Michel <143432891+MN97490@users.noreply.github.com>
    Date:   Mon Sep 23 08:58:55 2024 -0400
    
        Merge pull request #8 from projetV3R/V3R-VA-18/feature-
    
        V3 r va 18/feature
    
    commit c6507085d9e94d8f6bfca966eb7afe8daa2559cd
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 08:57:26 2024 -0400
    
        Responsive fait
    
    commit ace6b20e1d35c2ba95282b482cc1d40cfb0129ab
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Mon Sep 23 08:17:33 2024 -0400
    
        Ajout emplacement listes fournisseurs et emplacement insc/tp/filter
    
    commit f5e5448a388c0ae3b46aee84a2abbada3529997a
    Author: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>
    Date:   Sun Sep 22 22:53:46 2024 -0400
    
        Ajout debut layout page d'accueil section charts debut manque responsive

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,17 @@ Route::get('/dashboard',
 [UsagerController::class, 'dashboard'])->name('dashboard')->middleware('auth');
 
 Route::get('/redirection', function () {
-    return view('redirection');});
+    return view('redirection.403');});
+
     
 
     
 Route::get('/admin', function () {
     return view('admin.admin');});
+
     
+
+
+    //!!! ROUTE DE REDIRECTION ERREUR 404 TOUJOURS A LA FIN DU FICHIER DE ROUTE NE JAMAIS AVOIR DE ROUTE APRES !!!!
+ Route::fallback(function () {
+        return response()->view('redirection.404', [], 404);});
