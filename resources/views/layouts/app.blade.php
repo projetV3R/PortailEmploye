61ac73cb3a15a539f7df0de3ffde2f43293c643b
Mousse <143189920+IamMousse@users.noreply.github.com>
2024-11-20 17:05:57 -0500

    V3 r va 54/feature  (#21)
    * creation class daltonien
    
    * dalto
    
    * update daltonien
    
    bug laravel détect pad quand ispressed
    
    * update daltonien
    
    bug execute pas daltonien
    
    * update daltonien
    
    bug tjrs présent
    
    * update daltonien
    
    * update daltonien
    
    * update daltonien
    
    test- sur le bouton qui ne veux pas s'exécuter
    
    * fix
    
    * update daltonien
    
    lorsque début les 2 modes désactivité.
    
    * update daltonien
    
    nav barre + responsive nav barre ok
    
    * update daltonien
    
    page admin utilisateur ok
    manque:
    page admin paramètres
    page admin courriels
    page fournisseurs
    
    * update daltonien page login
    
    * update daltonien
    
    panneau admin terminer
    manque fournisseur
    
    * update daltonien
    
    retouche page login
    
    * update daltonien
    
    page fournisseurs
    
    * update daltonien fournisseur
    
    * daltonien fini
    
    attente avis nathan
    
    * delete console.log
    
    ---------
    
    Co-authored-by: Nathan Michel <nathan.michel.01@edu.cegeptr.qc.ca>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr" class="dark">
+<html lang="fr" class="dark daltonien">
 
 <head>
     <meta charset="utf-8" />
@@ -15,16 +15,15 @@
 </head>
 
 
-<body class="flex flex-col h-screen w-full bg-white dark:bg-gray-900 text-black dark:text-white">
+<body class="flex flex-col h-screen w-full text-black bg-white dark:bg-gray-800 dark:text-white daltonien:bg-black-400">
     <!-- HEADER -->
-
     <header>
 
         @yield('header')
-        <div class="flex w-full bg-blueV3R h-32 items-center justify-between p-4 relative">
+        <div class="flex w-full bg-blueV3R h-32 items-center justify-between p-4 relative ">
             <div class="flex items-center">
                 <a href="/dashboard">
-                    <img class="bg-white w-24 h-20 lg:hidden cursor-pointer"
+                    <img class="bg-white w-24 h-20 lg:hidden cursor-pointer daltonien:hover:bg-daltonienYellow daltonien:hover:text-black"
                         src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/ce/Logo_de_Trois-Rivi%C3%A8res_2022.png/600px-Logo_de_Trois-Rivi%C3%A8res_2022.png?20220917132718"
                         alt="Logo Trois-Rivières">
                 </a>
@@ -34,16 +33,21 @@
 
             <div class="md:hidden flex items-center text-white gap-2">
                 @auth
-                    <div>
-                        <button id="menu-toggle">
-                            <span class="iconify size-10 hover:animate-bounce " data-icon="mdi:menu"
-                                data-inline="false"></span>
-                        </button>
-                    </div>
+                <div>
+                    <button id="menu-toggle" class="daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                        <span class="iconify size-10 hover:animate-bounce " data-icon="mdi:menu"
+                            data-inline="false"></span>
+                    </button>
+                </div>
                 @endauth
                 <div>
-                    <button id="dark-mode-toggle" class=" hover:animate-pulse">
+                    <button id="dark-mode-toggle" class=" hover:animate-pulse daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
                         <span class="iconify size-10 " data-icon="circum:dark" data-inline="false"></span>
+                    </button>
+                </div>
+                <div>
+                    <button id="daltonien-mode-toggle" class="text-white hover:bg-green-300 hover:text-black daltonien:bg-daltonienYellow daltonien:text-black">
+                        <span class="iconify size-10" data-icon="material-symbols:contrast" data-inline="false"></span>
                     </button>
                 </div>
 
@@ -52,47 +56,59 @@
 
             <!-- Liens de navigation pour tablette minimum a revoir -->
             @auth
-                <nav
-                    class="hidden md:flex justify-center w-full text-lg xl:text-2xl items-center gap-4 text-white dark:text-white">
-                    <a href="/dashboard"
-                        class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg">Accueil</a>
-                    |
-                    @role('admin')
-                        <a href="/admin"
-                            class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg">Panneau
-                            Administration</a> |
-                    @endrole
-                    <a href="{{ route('fiches.index') }}"
-                        class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg">Fournisseurs</a>
-                    |
-                    <a href="/profil"
-                        class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg">Profil</a>
-                </nav>
+            <nav
+                class="hidden md:flex justify-center w-full text-lg xl:text-2xl items-center gap-4 text-white dark:text-white">
+                <a href="/dashboard"
+                    class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                    Accueil
+                </a>
+                |
+                @role('admin')
+                <a href="/admin"
+                    class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                    Panneau
+                    Administration
+                </a> 
+                |
+                @endrole
+                <a href="{{ route('fiches.index') }}"
+                    class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                    Fournisseurs
+                </a>
+                |
+                <a href="/profil"
+                    class="hover:bg-green-300 hover:text-black p-2 transition duration-300 ease-in-out transform hover:shadow-lg daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                    Profil
+                </a>
+            </nav>
             @endauth
 
             <!-- Deconnexion et Dark Mode desktop-->
             <div class="hidden md:flex space-x-4 items-center">
                 @auth
-                    <form class="deconnexionBtn" action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <div class="flex items-center space-x-4 hover:animate-bounce">
-                            <button class="ml-4 text-white">
-                                <span class="iconify size-8 lg:size-10" data-icon="mdi:logout" data-inline="false"
-                                    onclick="removeItemsLocalStorage()"></span>
-                            </button>
-                        </div>
-                    </form>
+                <form class="deconnexionBtn" action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <div class="flex items-center space-x-4 hover:animate-bounce daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                        <button class="ml-4 text-white">
+                            <span class="iconify size-8 lg:size-10" data-icon="mdi:logout" data-inline="false"
+                                onclick="removeItemsLocalStorage()"></span>
+                        </button>
+                    </div>
+                </form>
                 @endauth
 
-                <button id="dark-mode-toggle-desktop" class="text-white hover:animate-pulse  ">
+                <button id="dark-mode-toggle-desktop" class="text-white hover:animate-pulse daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
                     <span class="iconify size-8 lg:size-10" data-icon="circum:dark" data-inline="false"></span>
+                </button>
+                <button id="daltonien-mode-toggle-desktop" class="text-white hover:bg-green-300 hover:text-black daltonien:bg-daltonienYellow daltonien:text-black">
+                    <span class="iconify size-8 lg:size-10" data-icon="material-symbols:contrast" data-inline="false"></span>
                 </button>
             </div>
 
 
             <div class="hidden lg:flex absolute top-5 left-20 ">
                 <a href="/dashboard">
-                    <img class="w-36 h-36 bg-white shadow-lg"
+                    <img class="w-36 h-36 bg-white shadow-lg daltonien:hover:bg-daltonienYellow daltonien:hover:text-black"
                         src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/ce/Logo_de_Trois-Rivi%C3%A8res_2022.png/600px-Logo_de_Trois-Rivi%C3%A8res_2022.png?20220917132718"
                         alt="Logo Trois-Rivières Desktop">
                 </a>
@@ -107,38 +123,45 @@
                 <div class="flex items-center w-full">
                     <div class="flex justify-start w-full">
                         <a href="/dashboard">
-                            <img class="bg-white w-28 h-28 cursor-pointer"
+                            <img class="bg-white w-28 h-28 cursor-pointer daltonien:hover:bg-daltonienYellow daltonien:hover:text-black"
                                 src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/ce/Logo_de_Trois-Rivi%C3%A8res_2022.png/600px-Logo_de_Trois-Rivi%C3%A8res_2022.png?20220917132718"
                                 alt="Logo Trois-Rivières">
                         </a>
                     </div>
                     <!-- Bouton pour fermer le menu mobile -->
                     <div class="flex justify-end w-full">
-                        <button id="close-menu" class="text-white justify-end ">
-                            <span class="iconify size-10 hover:bg-green-300" data-icon="mdi:close"
+                        <button id="close-menu" class="text-white justify-end">
+                            <span class="iconify size-10 hover:bg-green-300 daltonien:hover:bg-daltonienYellow daltonien:hover:text-black" data-icon="mdi:close"
                                 data-inline="false"></span>
                         </button>
                     </div>
                 </div>
                 <!-- Liens de navigation pour mobile -->
                 <nav class="space-y-4 mt-4 text-white text-xl flex flex-col h-full">
-                    <a href="/dashboard" class="block hover:bg-green-300 p-2 transition duration-300">Accueil</a>
+                    <a href="/dashboard" class="block hover:bg-green-300 p-2 transition duration-300 daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                        Accueil
+                    </a>
                     @role('admin')
-                        <a href="/admin" class="block hover:bg-green-300 p-2 transition duration-300">Panneau
-                            Administration</a>
+                    <a href="/admin" class="block hover:bg-green-300 p-2 transition duration-300 daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                        Panneau
+                        Administration
+                    </a>
                     @endrole
-                    <a href="/fournisseurs"
-                        class="block hover:bg-green-300 p-2 transition duration-300">Fournisseurs</a>
-                    <a href="/profil" class="block hover:bg-green-300 p-2 transition duration-300">Profil</a>
+                    <a href="/fournisseurs" class="block hover:bg-green-300 p-2 transition duration-300 daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                        Fournisseurs
+                    </a>
+                    <a href="/profil" class="block hover:bg-green-300 p-2 transition duration-300 daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                        Profil
+                    </a>
 
                     <!-- Bouton deconnexion pour mobile -->
                     @auth
-                        <form action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button class="block text-white hover:bg-red-600 p-2 transition duration-300">
-                                <span class="iconify size-10" data-icon="mdi:logout" data-inline="false"></span> Déconnexion
-                            </button>
-                        </form>
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button class="block text-white hover:bg-red-600 p-2 transition duration-300 daltonien:hover:bg-daltonienYellow daltonien:hover:text-black">
+                            <span class="iconify size-10" data-icon="mdi:logout" data-inline="false"></span> Déconnexion
+                        </button>
+                    </form>
                     @endauth
                     <div class="flex h-full justify-center">
                         <div class="flex items-end text-xl">
@@ -150,6 +173,7 @@
         </div>
 
     </header>
+
 
     @yield('contenu')
 
@@ -166,6 +190,7 @@
             mobileMenu.classList.add('-translate-x-full');
         });
         @endauth
+
         // Fonction pour les deux boutons darkMode
         const toggleDarkMode = () => {
             const htmlElement = document.documentElement;
@@ -173,19 +198,44 @@
                 htmlElement.classList.remove('dark');
                 localStorage.setItem('theme', 'light');
             } else {
+                htmlElement.classList.remove('daltonien');
                 htmlElement.classList.add('dark');
                 localStorage.setItem('theme', 'dark');
             }
-        }
+        };
 
+        const toggleDaltonienMode = () => {
+            const htmlElement = document.documentElement;
+            if (htmlElement.classList.contains('daltonien')) {
+                htmlElement.classList.remove('daltonien');
+                localStorage.setItem('theme', 'light');
+            } else {
+                htmlElement.classList.remove('dark');
+                htmlElement.classList.add('daltonien');
+                localStorage.setItem('theme', 'daltonien');
+            }
+        };
+
+        //mode dark
         document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
         document.getElementById('dark-mode-toggle-desktop').addEventListener('click', toggleDarkMode);
 
+        // mode daltonien
+        document.getElementById('daltonien-mode-toggle').addEventListener('click', toggleDaltonienMode);
+        document.getElementById('daltonien-mode-toggle-desktop').addEventListener('click', toggleDaltonienMode);
+
         const savedTheme = localStorage.getItem('theme');
-        if (savedTheme && savedTheme === 'dark') {
+        if (savedTheme === 'dark') {
             document.documentElement.classList.add('dark');
+            document.documentElement.classList.remove('daltonien');
         } else {
             document.documentElement.classList.remove('dark');
+        }
+        if (savedTheme === 'daltonien') {
+            document.documentElement.classList.add('daltonien');
+            document.documentElement.classList.remove('dark');
+        } else {
+            document.documentElement.classList.remove('daltonien');
         }
 
         function removeItemsLocalStorage() {
